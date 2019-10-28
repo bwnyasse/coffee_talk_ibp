69a9c83b19d592e4e99a1227a7e0115503e968d6
bwnyasse <risbonyasse@gmail.com>
2019-10-28 22:05:51 +0100

    Step 7 - TODO

---
+++ lib/src/views/fav.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ToBeRemove {
+  //TODO 1- Create Second page StatelessWidget  : FavPage
+}
+
+/*
+ Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorites'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Page 2"),
+        ),
+      ),
+    );
+ */
