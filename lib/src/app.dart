382ece485b0bc51e48e55607dc51f0fa3838c68d
bwnyasse <risbonyasse@gmail.com>
2019-10-28 18:13:36 +0100

    Step2 - TODO

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,27 +1,15 @@
-
 import 'package:coffee_talk_ibp/src/views/home.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      //TODO: Update Title ->  Coffee Talk i-BP
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Coffee Talk i-BP'),
     );
   }
 }
